8f56a5216f33265ed800dd5607ec063cf5a35d19
George "༼ つ ◕_◕ ༽つ" Griffin <george@griff.in>
2015-02-06 14:58:46 -0500

    Adds oh-my-zsh

---
+++ .oh-my-zsh/custom/example.zsh
@@ -0,0 +1,9 @@
+# Add yourself some shortcuts to projects you often work on
+# Example:
+#
+# brainstormr=/Users/robbyrussell/Projects/development/planetargon/brainstormr
+#
+alias activate='source ~/Code/venvs/verificient/bin/activate'
+alias runserver='python manage.py runserver 0.0.0.0:8000'
+alias prettybg="google-chrome --app='http://8bitdash.com/'"
+alias notes='vi /home/george/Notes/"`date +\"%Y-%m-%d\"`".txt'
